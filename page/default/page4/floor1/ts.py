cb9075c3bf3c72ec97007aab3191e6b21388b0e8
Dustedtime <113445791+Dustedtime@users.noreply.github.com>
2023-06-26 23:54:58 +0800

    Add files via upload

--- page/default/page4/floor1/ts.py
+++ page/default/page4/floor1/ts.py
@@ -3,9 +3,9 @@ import json
 with open('monster.json','r') as f:
     data = json.load(f)
 
-for i in range(len(data)):
-    for j in range(len(data[i]['images'])):
-        data[i]['images'][j][3] = "2"
+for i in range(2):
+    for j in range(16):
+        data[i]['images'][j][3]="2"
 
 with open('monster.json','w') as f:
     json.dump(data, f)
